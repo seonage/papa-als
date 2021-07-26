45fe9d69e4154549617c358cd0b6f4d955cb441c
Steven Lin <steven.lin1986@gmail.com>
2021-07-26 17:28:57 -0400

    Added parent div tags in Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,16 +6,18 @@ import "../styling/styles.scss"
 
 const Navbar = () => {
     return (
-    <nav id="navbar">
-      <div id="headerlogo"><StaticImage alt="" src="../images/papaals.png"/></div>
-        <ul>
-          <li><a href="https://chicken-and-waffles-rockaway-nj.square.site/">Order</a></li>
-          <li><Link to="/">Menu</Link></li>
-          <li><Link to="/">Location</Link></li>
-          <li><Link to="/">About Us</Link></li>
-        </ul>
+    <div>
+      <nav id="navbar">
+        <div id="headerlogo"><StaticImage alt="" src="../images/papaals.png"/></div>
+            <ul>
+              <li><a href="https://chicken-and-waffles-rockaway-nj.square.site/">Order</a></li>
+              <li><Link to="/">Menu</Link></li>
+              <li><Link to="/">Location</Link></li>
+              <li><Link to="/">About Us</Link></li>
+          </ul>
+      </nav>
       <div id="mobile-menu">Hello</div>
-    </nav>
+    </div>
     )
 }
 
