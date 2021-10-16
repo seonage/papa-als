89066c3b81a24c23e6d813abd1ae13946bb9bc7a
Steven Lin <steven.lin1986@gmail.com>
2021-10-16 17:36:16 -0400

    Change phone number

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => {
         <div id="addressbox">
           286 Route 46<br/>
           Rockaway, NJ 07866<br/>
-          <div id="phonenumber">(201)-575-0599</div>
+          <div id="phonenumber">(201)-574-0599</div>
           <a href="https://chicken-and-waffles-rockaway-nj.square.site/">
             <div class="order">Order</div>
           </a>
