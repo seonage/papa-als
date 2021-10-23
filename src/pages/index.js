2c75d6a50aa28fcde2103182336e4469741c04a9
Steven Lin <steven.lin1986@gmail.com>
2021-10-22 22:15:50 -0400

    Family meals

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,7 @@ const IndexPage = () => {
             <div class="order">Order</div>
           </a>
         </div>
-        <div id="itemsdisplay"><StaticImage alt="" src="../images/itemsdisplay.png"/></div>
-        <div id="tenders"><StaticImage alt="" src="../images/tenders.JPG"/></div>
+        <div id="itemsdisplay"><StaticImage alt="" src="../images/familymeals.jpg"/></div>
       </div>
       <div id="foodphilosophysection">
         <div id="foodphilosophylogo"><StaticImage alt="" src="../images/foodphilosophy.png"/></div>
